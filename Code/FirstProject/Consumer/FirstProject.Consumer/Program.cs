96e967dbca679d87b2795c41b9defac8c0fd228a
tajoreh <tajoreh@gmail.com>
2023-10-25 12:46:36 +0330

    #first project pushed

---
+++ Code/FirstProject/Consumer/FirstProject.Consumer/Program.cs
@@ -0,0 +1,38 @@
+﻿//6. Create a connection
+
+using System.Reflection;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+
+//7. Create a channel
+using var channel = connection.CreateModel();
+
+//8. Declare a queue
+channel.QueueDeclare(
+    queue: "LetterBox",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+//9. Create a consumer which listens through channel
+var consumer=new EventingBasicConsumer(channel);
+
+//10. Create a callback for when it receives the message
+consumer.Received += (model, eventArgs) =>
+{
+    var body=eventArgs.Body.ToArray();
+    var message=Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Message Received: {message}");
+
+};
+
+
+//11. Consumes to the messages
+channel.BasicConsume(queue:"LetterBox",autoAck:true,consumer:consumer);
+
+Console.ReadKey();
---
+++ Code/FirstProject/Producer/FirstProject.Producer/Program.cs
@@ -0,0 +1,25 @@
+﻿//1. Create a connection
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+
+//2. Create a channel
+using var channel=connection.CreateModel();
+
+//3. Declare a queue
+channel.QueueDeclare(
+    queue:"LetterBox",
+    durable:false,
+    exclusive:false,
+    autoDelete:false,
+    arguments:null);
+
+//4. Create a message
+var message="This is my first Message";
+var encodedMessage=Encoding.UTF8.GetBytes(message);
+
+//5. Publish the message: "" means default exchange
+channel.BasicPublish(exchange:"",routingKey:"LetterBox",basicProperties:null,body:encodedMessage);
+
